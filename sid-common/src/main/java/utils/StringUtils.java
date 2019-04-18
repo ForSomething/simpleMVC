25a9e0b5aef268cb3f6a2fac9b7b03e03a294941
lizhaoxing <lzx_softbank@126.com>
2019-04-18 18:44:34 +0800

    123

--- sid-common/src/main/java/utils/StringUtils.java
+++ sid-common/src/main/java/utils/StringUtils.java
@@ -5,14 +5,15 @@ import org.apache.commons.beanutils.BeanUtils;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class StringUtils {
     static public String emptyString = "";
 
     static public byte[] GetByteArray(String str,String charsetName) throws UnsupportedEncodingException {
         if(str != null){
-            byte[] byteArray = str.getBytes(charsetName);
-            return byteArray;
+            return str.getBytes(charsetName);
         }
         return null;
     }
@@ -25,11 +26,69 @@ public class StringUtils {
         return str == null || str.trim().equals("");
     }
 
-    static public <T> T getValueByTemplate(String srcString,String templateString,Class disObjectClass) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+    static public <T> T getValueByTemplate(String srcString,String templateString,Class disObjectClass) throws Exception {
+        if(isNullOrWihtespace(templateString)){
+            return null;
+        }
         T disObject = (T)disObjectClass.newInstance();
         HashMap<String,Object> valueMap = new HashMap<>();
-        
+        SyntaxTree syntaxTree = new SyntaxTree(templateString);
         BeanUtils.populate(disObject,valueMap);
         return disObject;
+    }
+}
+
+class SyntaxTree{
+    private static SyntaxTree rulesSyntaxTree;
+
+    TreeNode root;
+
+    static {
+        rulesSyntaxTree = new SyntaxTree();
+        rulesSyntaxTree.root = new TreeNode();
+        rulesSyntaxTree.root.value = '#';
+        rulesSyntaxTree.root.children = new LinkedList<>();
+        rulesSyntaxTree.root.children.add(null);
+    }
+
+    private SyntaxTree(){}
+
+    public SyntaxTree(String statement){
+        ParsedStatement parsedStatement = new ParsedStatement(statement);
+        root = new TreeNode();
+        parsedStatement.nextChar();
+        root.value = parsedStatement.currentChar();
+        addChildren(root,parsedStatement);
+    }
+
+    private void addChildren(TreeNode node,ParsedStatement parsedStatement){
+
+    }
+
+    private class ParsedStatement{
+        private String statement;
+        private int point;
+
+        public ParsedStatement(String statement){
+            point = -1;
+            this.statement = statement;
+        }
+
+        public boolean nextChar(){
+            return ++point == statement.length();
+        }
+
+        public char currentChar(){
+            return statement.charAt(point);
+        }
+    }
+}
+
+class TreeNode{
+    char value;
+    List<TreeNode> children;
+
+    public boolean hasChildren(){
+        return children == null || children.size() == 0;
     }
 }
